21ae2b919d01f12569095c97bb2a972d7b44ac61
lorenzorru0 <lorenzoorru999@gmail.com>
2021-11-10 16:03:57 +0100

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.base')
+
+@section('mainContent')
+    <h1>Backoffice Comics</h1>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,16 @@
+<header>
+    <ul class="nav justify-content-center">
+        <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Active</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+        </li>
+    </ul>
+</header>

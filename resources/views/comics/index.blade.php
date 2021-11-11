31fe53f61ca9c129002961c5f7c556f53aff789d
lorenzorru0 <lorenzoorru999@gmail.com>
2021-11-11 16:54:22 +0100

    Add delete feature

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -28,7 +28,34 @@
                     <td>
                         <a class="d-block mb-1" href="{{route('comics.show', $comic['id'])}}"><button type="button" class="btn btn-info">View</button></a><br>
                         <a class="d-block mb-1" href="{{route('comics.edit', $comic['id'])}}"><button type="button" class="btn btn-warning">Edit</button></a><br>
-                        <a class="d-block mb-1" href=""><button type="button" class="btn btn-danger">Delete</button></a><br>
+                        <form action="{{route('comics.destroy', $comic['id'])}}" method="POST">
+                            @csrf
+                            @method('DELETE')
+
+                            <!-- Button trigger modal -->
+                            <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#staticBackdrop">Delete</button>
+
+                            <!-- Modal -->
+                            <div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="staticBackdropLabel">Delete comic validation</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    Are you sure that you want delte {{$comic['title']}}
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    <button type="submit" class="btn btn-danger">Delete</button>
+                                </div>
+                                </div>
+                            </div>
+                            </div>                           
+                        </form>
                     </td>
                 </tr>
             @endforeach
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,4 +2,5 @@
 
 @section('mainContent')
     <h1>Backoffice Comics</h1>
+    <img src="{{asset('images/dc-logo.png')}}" alt="">
 @endsection
